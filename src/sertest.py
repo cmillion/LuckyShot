e785649aa20fb333f1e956bcefefec4eb08a1f3e
Chase Million <chase.million@gmail.com>
2014-10-06 12:29:48 -0400

    functionalize stuff

--- src/sertest.py
+++ src/sertest.py
@@ -3,40 +3,55 @@
 %pylab
 import struct
 
+def readheader(filename):
+    header = {}
+    with open(filename,'rb') as f:
+        header['FileID'] = f.read(14) #str
+        # Do an incredibly naive sanity check on the data
+        if not header['FileID'] == 'LUCAM-RECORDER':
+            print 'This is not a .ser formatted file.'
+        header['LuID'] = struct.unpack('<I',f.read(4))[0] #int32
+        header['ColorID'] = struct.unpack('<I',f.read(4))[0] #int32
+        header['LittleEndian'] = struct.unpack('<I',f.read(4))[0] #int32
+        header['ImageWidth'] = struct.unpack('<I',f.read(4))[0] #int32
+        header['ImageHeight'] = struct.unpack('<I',f.read(4))[0] #int32
+        header['PixelDepth'] = struct.unpack('<I',f.read(4))[0] #int32
+        header['BytePerPixel'] = 1 if PixelDepth<=8 else 2
+        header['FrameCount'] = struct.unpack('<I',f.read(4))[0] #int32
+        header['Observer'] = struct.unpack('<40s',f.read(40))[0] #str
+        header['Instrument'] = struct.unpack('<40s',f.read(40))[0] #str
+        header['Telescope'] = struct.unpack('<40s',f.read(40))[0] #str
+        header['DateTime'] = struct.unpack('<8s',f.read(8))[0] #"Date"
+        header['DateTime_UTC'] = struct.unpack('<8s',f.read(8))[0] #"Date"
+
+    return header
+
+def readframe(filename,frame,header=False):
+    if not header:
+        header = readheader(filename)
+    if frame > header['FrameCount']-1:
+        print 'Frame #{frame} requested of {count} available.'.format(
+                                                frame=frame, count=FrameCount)
+        return False
+    HeaderBytes = 178
+    ImagePixels = header['ImageWidth']*header['ImageHeight']
+    ImageBytes = ImagePixels*header['BytePerPixel']
+    with open(filename,'rb') as f:
+        f.seek(HeaderBytes+ImageBytes*WhichFrame)
+        f.read(ImageBytes)
+        fmt = '{endian}{pixels}{fmt}'.format(
+                            endian='<' if header['LittleEndian'] else '>',
+                            pixels=ImagePixels,
+                            fmt='H' if BytePerPixel==2 else 'B')
+        img = np.array(struct.unpack(fmt,f.read(ImageBytes))).reshape(ImageHeight,ImageWidth)
+
+    return img, header
+
 # Change this filename to suit your needs...
 filename = './uranus20141002-1511.ser'
 
-with open(filename,'rb') as f:
-    FileID = f.read(14) #str
-    # Do an incredibly naive sanity check on the data
-    if not FileID == 'LUCAM-RECORDER':
-        print 'This is not a .ser formatted file.'
-    LuID = struct.unpack('<I',f.read(4))[0] #int32
-    ColorID = struct.unpack('<I',f.read(4))[0] #int32
-    LittleEndian = struct.unpack('<I',f.read(4))[0] #int32
-    ImageWidth = struct.unpack('<I',f.read(4))[0] #int32
-    ImageHeight = struct.unpack('<I',f.read(4))[0] #int32
-    PixelDepth = struct.unpack('<I',f.read(4))[0] #int32
-    BytePerPixel = 1 if PixelDepth<=8 else 2
-    FrameCount = struct.unpack('<I',f.read(4))[0] #int32
-    Observer = struct.unpack('<40s',f.read(40))[0] #str
-    Instrume = struct.unpack('<40s',f.read(40))[0] #str
-    Telescope = struct.unpack('<40s',f.read(40))[0] #str
-    DateTime = struct.unpack('<8s',f.read(8))[0] #"Date"
-    DateTime_UTC = struct.unpack('<8s',f.read(8))[0] #"Date"
-
 print 'Image size is: '+str(ImageWidth)+'x'+str(ImageHeight)
 print 'Frame count: '+str(FrameCount)
-
-HeaderBytes = 178
-WhichFrame = 150
-ImagePixels = ImageWidth*ImageHeight
-ImageBytes = ImagePixels*BytePerPixel
-with open(filename,'rb') as f:
-    f.seek(HeaderBytes+ImageBytes*WhichFrame)
-    f.read(ImageBytes)
-    fmt = '<'+str(ImagePixels)+('H' if BytePerPixel==2 else 'B')
-    img = np.array(struct.unpack(fmt,f.read(ImageBytes))).reshape(ImageHeight,ImageWidth)
 
 plt.imshow(img)
 
