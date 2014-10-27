40d206080e7c0aa32fc18a65e63eec5db014dd26
Chase Million <moc.liamg@noillim.esahc>
2014-10-27 13:15:37 -0400

    fix endian handling

--- src/ser.py
+++ src/ser.py
@@ -6,9 +6,6 @@ def readheader(filename):
     header = {}
     with open(filename,'rb') as f:
         header['FileID'] = f.read(14) #str
-        # Do an incredibly naive sanity check on the data
-        if not header['FileID'] == 'LUCAM-RECORDER':
-            print 'This is not a .ser formatted file.'
         header['LuID'] = struct.unpack('<I',f.read(4))[0] #int32
         header['ColorID'] = struct.unpack('<I',f.read(4))[0] #int32
         header['LittleEndian'] = struct.unpack('<I',f.read(4))[0] #int32
@@ -22,8 +19,18 @@ def readheader(filename):
         header['Telescope'] = struct.unpack('<40s',f.read(40))[0] #str
         header['DateTime'] = struct.unpack('<8s',f.read(8))[0] #"Date"
         header['DateTime_UTC'] = struct.unpack('<8s',f.read(8))[0] #"Date"
-
+    print '{f} - {d}'.format(f=filename,d=header['DateTime'])
+    print '    Dims: {h}x{w}'.format(
+                                w=header['ImageWidth'],h=header['ImageHeight'])
+    print '    Frames: {n}'.format(n=header['FrameCount'])
     return header
+
+def readtrailer(filename):
+    header = readheader(filename)
+    offset = (header['FrameCount'] * header['ImageWidth'] *
+              header['ImageHeight'] * header['BytePerPixel'])
+    trailer = []
+    return trailer
 
 def readframe(filename,frame,header=False):
     if not header:
@@ -39,11 +46,13 @@ def readframe(filename,frame,header=False):
         f.seek(HeaderBytes+ImageBytes*frame)
         f.read(ImageBytes)
         fmt = '{endian}{pixels}{fmt}'.format(
-                            endian='<' if header['LittleEndian'] else '>',
+                            endian='>' if header['LittleEndian'] else '<',
                             pixels=ImagePixels,
                             fmt='H' if header['BytePerPixel']==2 else 'B')
         img = np.array(struct.unpack(fmt,f.read(ImageBytes))).reshape(
                                     header['ImageHeight'],header['ImageWidth'])
 
-    return img, header
-
+    if not header:
+        return img, header
+    else:
+        return img
